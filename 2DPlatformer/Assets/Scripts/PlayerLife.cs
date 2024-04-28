0b6138c4da5c3778bae52a3c4180c2b7b5a348aa
Nikolas Schodl <if23b142@technikum-wien.at>
2024-04-28 18:24:48 +0200

    Added a health system and fixed the colliders of the floating islands. Also I just wanted to add some spikes to the bottom, lol.

--- 2DPlatformer/Assets/Scripts/PlayerLife.cs
+++ 2DPlatformer/Assets/Scripts/PlayerLife.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private int hp = 1;
+    private bool dead = false;
+
+    [SerializeField] private Text HPText;
 
     private void Start()
     {
@@ -14,11 +19,26 @@ public class PlayerLife : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        if(hp < 1 && !dead)
+        {
+            Die();
+            dead = true;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
-            Die();
+            hp--;
+            HPText.text = "HP: " + hp;
+        }
+        if (collision.gameObject.CompareTag("InstaKillTrap"))
+        {
+            hp = 0;
+            HPText.text = "HP: " + hp;
         }
     }
 
@@ -31,5 +51,15 @@ public class PlayerLife : MonoBehaviour
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Wenn das Objekt den richtigen Tag hat
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            hp++;
+            HPText.text = "HP: " + hp;
+        }
     }
 }
