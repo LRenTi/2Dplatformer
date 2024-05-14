4be850eb3cddd43efdd78afa097bdd5596e51dd9
test <test.com>
2024-05-14 14:10:34 +0200

    Checkpoints Created, but not working for unknown reason

---
+++ 2DPlatformer/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Checkpointmaster GM;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            GM.lastCheckPointPos = transform.position;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GameObject.FindGameObjectWithTag("GM").GetComponent<Checkpointmaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
