100c2265c8a335c79138afc00af5fc4f355cb547
Samuel Hammerschmidt <samuel.hammerschmidt@gmx.at>
2024-05-28 13:41:33 +0200

    Level selection, start & end menu bugs to fix

--- 2DPlatformer/Assets/Scripts/EndMenu.cs
+++ 2DPlatformer/Assets/Scripts/EndMenu.cs
@@ -6,6 +6,7 @@ public class EndMenu : MonoBehaviour
 {
     public void Quit()
     {
+        PlayerPrefs.DeleteAll();
         Application.Quit();
     }
 }
--- 2DPlatformer/Assets/Scripts/Finish.cs
+++ 2DPlatformer/Assets/Scripts/Finish.cs
@@ -7,10 +7,15 @@ public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
     private bool levelCompleted = false;
+    public GameObject player;
+
+    private Checkpointmaster checkpointMaster; // Reference to the Checkpointmaster
+
     // Start is called before the first frame update
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
+        checkpointMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<Checkpointmaster>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,12 +24,28 @@ public class Finish : MonoBehaviour
         {
             finishSound.Play();
             levelCompleted = true;
+            UnlockNewLevel();
             Invoke("CompleteLevel", 2f);
+        }
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
         }
     }
 
     private void CompleteLevel()
     {
+        // Reset the checkpoint position before loading the new level
+        if (checkpointMaster != null)
+        {
+            checkpointMaster.ResetCheckpoint();
+        }
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
